a87674431b4668b13de1884242cf0b4114fedb2d
Fahri Firdausillah <elfaatta@gmail.com>
2015-11-23 16:57:52 +0700

    model and requirements

--- project_man/board/models.py
+++ project_man/board/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class Backlog(models.Model):
+    end_user = TaggableManager()
+    component = models.CharField(max_length=200)
+    parent = TreeForeignKey('self', null=True, blank=True, related_name='children', db_index=True)
+    data = models.TextField(null=True, blank=True)
+    testing = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    label = TaggableManager()
+
+    class Meta:
+        order_insertion_by = ['component']
--- project_man/settings.py
+++ project_man/settings.py
@@ -30,12 +30,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'suit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit',
+    'mptt',
+    'board',
+
 )
 
 MIDDLEWARE_CLASSES = (
