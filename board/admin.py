c8e263526f86a47d291b8865f9b2734739dabc26
Fahri Firdausillah <elfaatta@gmail.com>
2015-11-24 11:31:43 +0700

    text widget

--- board/admin.py
+++ board/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
 from mptt.admin import MPTTModelAdmin
 from suit.admin import SortableModelAdmin
+from suit.widgets import AutosizedTextarea
+from django.forms import ModelForm
 
 from .models import Board, Backlog
+
+
+class BacklogForm(ModelForm):
+    class Meta:
+        widgets = {
+            'description': AutosizedTextarea(attrs={'class': 'span12'}),
+            'data': AutosizedTextarea(attrs={'class': 'span12'}),
+            'testing': AutosizedTextarea(attrs={'class': 'span12'}),
+        }
 
 
 class BacklogInline(admin.StackedInline):
     model = Backlog
     fields = ('component', 'description', 'data', 'testing', 'status')
     extra = 1
+    form = BacklogForm
 
 
 class BoardAdmin(admin.ModelAdmin):
@@ -22,18 +34,19 @@ class BacklogAdmin(MPTTModelAdmin):
     list_filter = ('board__title', 'status', 'component')
     search_fields = ['component']
     mptt_level_indent = 15
+    form = BacklogForm
 
-    def suit_row_attributes(self, obj, request):
-        class_map = {
-            'Finished': 'success',
-            'In Progress': 'warning',
-            'Not Ready': 'error',
-            'Ready to Test': 'info',
-        }
-
-        css_class = class_map.get(obj.status)
-        if css_class:
-            return {'class': css_class}
+#    def suit_row_attributes(self, obj, request):
+#        class_map = {
+#            'Finished': 'success',
+#            'In Progress': 'warning',
+#            'Not Ready': 'error',
+#            'Ready to Test': 'info',
+#        }
+#
+#        css_class = class_map.get(obj.status)
+#        if css_class:
+#            return {'class': css_class}
 
 
 admin.site.register(Board, BoardAdmin)
