6599e89772e3003fc39fcd373fa1723a65027d2c
Fahri Firdausillah <elfaatta@gmail.com>
2015-11-24 16:26:53 +0700

    collect static

--- project_man/settings.py
+++ project_man/settings.py
@@ -103,4 +103,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+MEDIA_ROOT = os.path.join(STATIC_ROOT, 'media/')
+MEDIA_URL = '/media/'
